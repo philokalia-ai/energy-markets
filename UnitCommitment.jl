20406120ef6a9ec5a2fad80deedcca7d6568a30f
yannnisgeo <john.k.georg@gmail.com>
2024-12-11 22:10:32 +0200

    refactor: introduce time dimension

--- UnitCommitment.jl
+++ UnitCommitment.jl
@@ -12,27 +12,32 @@ function solve_unit_commitment(
     # TODO: choose optimizer
     model = Model(HiGHS.Optimizer)
     set_silent(model)
+
     N = length(units[:, "unit"])
+    T = length(scenario.demand) # TBD
 
     # generation must be lower than maximum
-    @variable(model, 0 <= g[i = 1:N] <= units.p_max[i])
+    @variable(model, 0 <= g[i = 1:N, t = 1:T] <= units.p_max[i])
 
     # binary commitment variables 
-    @variable(model, u[i = 1:N], Bin)
+    @variable(model, u[i = 1:N, t = 1:T], Bin)
+
+    # binary startup variable
+    #@variable(model, v[i = 1:N, t = 1:T], Bin)
 
     # generation of commited units must be within limits
-    @constraint(model, [i = 1:N], g[i] <= units.p_max[i] * u[i])
-    @constraint(model, [i = 1:N], g[i] >= units.p_min[i] * u[i])
+    @constraint(model, [i = 1:N, t = 1:T], g[i, t] <= units.p_max[i, t] * u[i,t])
+    @constraint(model, [i = 1:N, t = 1:T], g[i, t] >= units.p_min[i, t] * u[i,t])
 
     # conventional Supply must equal Demand minus RES production
-    @constraint(model, sum(g[i] for i in 1:N) == scenario.demand - scenario.RES)
+    @constraint(model, sum(g[i, t] for i in 1:N, t in 1:T) == scenario.demand[t] - scenario.RES)
 
     @objective(
         model,
         Min,
         # currently random costs
-        sum(units.fixed_cost[i] * u[i] for i in 1:N) + 
-        sum(units.variable_cost[i] * g[i] for i in 1:N) 
+        sum(units.fixed_cost[i] * u[i, t] for i in 1:N, t in 1:T) + 
+        sum(units.variable_cost[i] * g[i, t] for i in 1:N, t in 1:T) 
     )
 
     optimize!(model)
@@ -51,7 +56,7 @@ function solve_unit_commitment(
 end
 
 units = get_generators(true)
-scenario = Scenario(62000.0, 1000.0)
+scenario = Scenario([62000.0, 63000.0, 61000.0, 58000.0, 67000.0] , 1000.0)
 
 solution = solve_unit_commitment(units, scenario)
 
